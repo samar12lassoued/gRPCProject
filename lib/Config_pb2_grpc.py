fb6a427d3af4b19de3759bddcabbfc87c10bce5b
SamarLassoued <73782851+samar12lassoued@users.noreply.github.com>
2022-05-03 11:00:30 +0100

    Update Config_pb2_grpc.py

--- lib/Config_pb2_grpc.py
+++ lib/Config_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import Config_pb2 as Config__pb2
+from lib import Config_pb2 as Config__pb2
 
 
 class ConfigManagerStub(object):
