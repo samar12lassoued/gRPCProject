c92785fd5d6dbd2b0a96c28487c1485fd61b729b
SamarLassoued <73782851+samar12lassoued@users.noreply.github.com>
2022-05-03 11:09:47 +0100

    Update mqtt_pb2_grpc.py

--- lib/mqtt_pb2_grpc.py
+++ lib/mqtt_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import mqtt_pb2 as mqtt__pb2
+from lib import mqtt_pb2 as mqtt__pb2
 
 
 class MQTTManagerStub(object):
